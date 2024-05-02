632deea3fedeb030b8b3a712908fb641c2dd8d2c
nitish <mandalnitish570@gmail.com>
2024-05-02 11:23:47 +0530

    1

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed = new Typed("#auto-type",{
+    strings: ["Frontend Devloper" , "Programmer" , "Designer" , "Developer"],
+    typeSpeed : 150,
+    backspeed : 150,
+    loop : true
+});
+
+var main = document.querySelector(".AllBody")
+var crsr = document.querySelector(".cursor")
+
+main.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+
+})
